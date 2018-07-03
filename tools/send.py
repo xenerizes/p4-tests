c6c7a022cbd9e96fbad8d4d6a8498692ea06c9bb
Yuliya <xenerizes@users.noreply.github.com>
2018-07-03 17:42:33 +0300

    Rewrite according to SwitchMonitor interface

--- tools/send.py
+++ tools/send.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from scapy.all import PacketList
 from scapy.all import Ether, TCP, IP
 from scapy.all import RandMAC, RandIP
 from scapy.all import get_if_hwaddr
@@ -9,9 +8,10 @@ from switch_monitor import SwitchMonitor
 
 
 def make_pkt(dst, iface):
-    pkt = Ether(src=get_if_hwaddr(iface), dst=("00:00:00:00:02:0"+dst))
+    pkt = Ether(src=get_if_hwaddr(iface), dst=("00:00:00:00:03:0"+dst))
     pkt = pkt/IP(dst="192.168.200.24")
-    pkt = pkt/TCP(dport=1234, sport=randint(49152,65535))/("load")
+    pkt = pkt/TCP(dport=1234, sport=randint(49152,65535))
+    pkt = pkt/("GET /path/resource?param1=value1&param2=value2 HTTP/1.1")
     return pkt
 
 port_map = {
@@ -25,10 +25,10 @@ local_port = 0
 sender = 1
 
 packet_map = {
-    1: PacketList(make_pkt('3', 'veth2'))
+    1: list(make_pkt('3', 'veth2'))
 }
 
 
-sm = SwitchMonitor(port_map, senders=[1], pkt_map=packet_map)
+sm = SwitchMonitor(port_map, pkt_map=packet_map)
 res = sm.run()
 print(res.data)
