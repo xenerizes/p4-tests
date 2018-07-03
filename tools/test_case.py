d30e052cf9fc5a1aef12c4c19fc52cae47713a26
Yuliya <xenerizes@users.noreply.github.com>
2018-07-03 18:25:34 +0300

    Docstrings

--- tools/test_case.py
+++ tools/test_case.py
@@ -2,6 +2,12 @@ from switch_monitor import SwitchMonitor
 
 
 def get_pkt_fields(pkt):
+    """
+    Extract all fields from the packet.
+
+    :type pkt: scapy non-built packet
+    :rtype: list of scapy layers
+    """
     def expand(pkt):
         yield pkt
         while pkt.payload:
@@ -12,6 +18,9 @@ def get_pkt_fields(pkt):
 
 
 def compare_pkt(rhs, lhs):
+    """
+    Compare two packets layer by layer
+    """
     rhs_fields, lhs_fields = (get_pkt_fields(x) for x in (rhs, lhs))
     for layer_rhs, layer_lhs in zip(rhs_fields, lhs_fields):
         if layer_lhs != layer_rhs:
@@ -19,7 +28,10 @@ def compare_pkt(rhs, lhs):
     return True
 
 
-def compare_pkt_map(rhs, lhs):
+def compare_pkt_list(rhs, lhs):
+    """
+    Compare two packet lists packet by packet, layer by layer
+    """
     for pkt_rhs, pkt_lhs in zip(rhs, lhs):
         if not compare_pkt(pkt_rhs, pkt_lhs):
             return False
@@ -38,7 +50,7 @@ class ScenarioTestCase(object):
     def run(self):
         monitor = SwitchMonitor(port_map=self.port_map, pkt_map=self.packet_map)
         self.captured_map = monitor.run().data
-        return compare_pkt_map(self.expected_map, self.captured_map)
+        return self.compare_pkt_maps()
 
     def summary(self):
         if self.captured_map is None:
@@ -46,3 +58,6 @@ class ScenarioTestCase(object):
         # TODO
         print('Expected: ', self.expected_map)
         print('Captured: ', self.captured_map)
+
+    def compare_pkt_maps(self):
+        pass
