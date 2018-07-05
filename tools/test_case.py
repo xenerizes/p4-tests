3091b31386504a4e5a429a8223a795187329938d
Yuliya <xenerizes@users.noreply.github.com>
2018-07-05 14:50:17 +0300

    Implementation of compare_pkt_maps method

--- tools/test_case.py
+++ tools/test_case.py
@@ -1,41 +1,16 @@
 from tools.switch_monitor import SwitchMonitor
 
 
-def get_pkt_fields(pkt):
+def compare_pkt_list(expected, captured):
     """
-    Extract all fields from the packet.
-
-    :type pkt: scapy non-built packet
-    :rtype: list of scapy layers
+    Compare two packet lists
     """
-    def expand(pkt):
-        yield pkt
-        while pkt.payload:
-            pkt = pkt.payload
-            yield pkt
-
-    return list(expand(pkt))
-
-
-def compare_pkt(rhs, lhs):
-    """
-    Compare two packets layer by layer
-    """
-    rhs_fields, lhs_fields = (get_pkt_fields(x) for x in (rhs, lhs))
-    for layer_rhs, layer_lhs in zip(rhs_fields, lhs_fields):
-        if layer_lhs != layer_rhs:
+    for pkt in expected:
+        if pkt not in captured:
             return False
-    return True
+        captured.remove(pkt)
 
-
-def compare_pkt_list(rhs, lhs):
-    """
-    Compare two packet lists packet by packet, layer by layer
-    """
-    for pkt_rhs, pkt_lhs in zip(rhs, lhs):
-        if not compare_pkt(pkt_rhs, pkt_lhs):
-            return False
-    return True
+    return len(captured) == 0
 
 
 class ScenarioTestCase(object):
@@ -51,7 +26,8 @@ class ScenarioTestCase(object):
     def run(self):
         monitor = SwitchMonitor(port_map=self.port_map, pkt_map=self.packet_map)
         self.captured_map = monitor.run().data
-        return self.compare_pkt_maps()
+        eq = self.compare_pkt_maps()
+        print("OK" if eq else "Failed")
 
     def summary(self):
         if self.captured_map is None:
@@ -61,4 +37,8 @@ class ScenarioTestCase(object):
         print('Captured: ', self.captured_map)
 
     def compare_pkt_maps(self):
-        pass
+        for port in self.expected_map:
+            if not compare_pkt_list(self.captured_map[port],
+                                    self.expected_map[port]):
+                return False
+        return True
