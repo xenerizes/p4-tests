ac4c22eea275385dfd47d8b1122f60cd8076902f
Yuliya <xenerizes@users.noreply.github.com>
2018-07-06 16:55:13 +0300

    Generate packets with identifiers if given

--- tools/packets.py
+++ tools/packets.py
@@ -7,10 +7,11 @@ MAC_MCAST_PREFIX = "01:00:5e:00:"
 MAC_STANDARD_PREFIX = "00:00:00:00:"
 
 
-def make_pkt(dst, iface, mcast=False):
+def make_pkt(dst, iface, mcast=False, id=None):
     prefix = MAC_MCAST_PREFIX if mcast else MAC_STANDARD_PREFIX
     pkt = Ether(src=get_if_hwaddr(iface), dst=(prefix + dst))
     pkt = pkt/IP(dst="192.168.200.24")
     pkt = pkt/TCP(dport=1234, sport=randint(49152,65535))
-    pkt = pkt/("GET /path/resource?param1=value1&param2=value2 HTTP/1.1")
+    if id is not None:
+        pkt = pkt/(id)
     return pkt
